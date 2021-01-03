cf085fdcb0c18c36d6bd6fa8c685eb4b0a151660
Matveibos <71291376+Matveibos@users.noreply.github.com>
2021-01-03 19:16:48 +0300

    Update Lab_6.cpp

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,20 +1,72 @@
-﻿// Lab_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+
+using namespace std;
+
+template <class X, class Y> int perevorot(X& length, Y& mas)
+{
+	cout << "here work template" << endl;
+	int i = 0;
+	while (i < length)
+	{
+		if (mas[i] == element)
+		{
+			int j = 0;
+			while (i + j < length - 1)
+			{
+				mas[i + j] = mas[i + j + 1];
+				j++;
+			}
+			cout << "length " << length << endl;
+			length = length - 1;
+			i--;
+		}
+		i++;
+	}
+	return length;
+}
+
+int deleteargs(int length, char* mas, char element)
+{
+	cout << "here work function (char*)" << endl;
+	int i = 0;
+	while (i < length)
+	{
+		if (mas[i] == element)
+		{
+			int j = 0;
+			while (i + j < length - 1)
+			{
+				mas[i + j] = mas[i + j + 1];
+				j++;
+			}
+			length = length - 1;
+			cout << "length " << length << endl;
+			i--;
+		}
+		i++;
+	}
+	return length;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x, i;
+	char z;
+	char* y;
+	cout << "Enter length" << endl;
+	cin >> x;
+	cout << "Enter delete element" << endl;
+	cin >> z;
+	y = new char[x];
+	cout << "Enter mas" << endl;
+	for (i = 0; i < x; i++)
+	{
+		cin >> y[i];
+	}
+	cout << endl;
+	x = deleteargs(x, y, z);
+	cout << "mas after delete" << endl;
+	for (i = 0; i < x; i++)
+		cout << y[i] << " ";
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
